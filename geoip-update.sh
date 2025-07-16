088b2d2730644ab1768be58ec1f69b8981b18702
An | Anton Röhm <18481195+AnTheMaker@users.noreply.github.com>
2025-07-16 15:17:08 +0200

    add support for ip-to-country and ip-to-asn databases

--- geoip-update.sh
+++ geoip-update.sh
@@ -3,15 +3,19 @@
 # https://db-ip.com/db/download/ip-to-city-lite
 #
 # ABOUT THIS SCRIPT:
-# This script gets the latest version of the free "IP-to-City Lite" GeoIP database provided by db-ip.com
+# This script gets the latest version of the free "IP-to-Country/City/ASN Lite" GeoIP database provided by db-ip.com
 # and places it at /var/geoip-data/geoip.mmdb (can be changed below)
 #
 # It is recommended to create a cronjob to execute this script once a month to keep your GeoIP database up-to-date
 # https://github.com/AnTheMaker/db-ip-update
 
 
+# change these variables to your liking
 geoip_dir="/var/geoip-data"
-download_file="dbip-city-lite-$(date +'%Y')-$(date +'%m').mmdb"
+type="city" # can be "city", "country", or "asn"
+
+
+download_file="dbip-$type-lite-$(date +'%Y')-$(date +'%m').mmdb"
 download_url="https://download.db-ip.com/free/$download_file.gz"
 
 # create destination folder (if it doesn't exist yet)
